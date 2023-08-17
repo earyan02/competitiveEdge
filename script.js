c6aa90381c464e809b06ceaf3e9d29d355d67398
earyan02 <earyan02@gmail.com>
2023-08-17 19:39:26 -0400

    adding JS

---
+++ script.js
@@ -0,0 +1,8 @@
+myFunction() {
+    var x = document.getElementById("moreInfo");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
