e4a80ab15cca4b2e16a469770aca1722751aa490
earyan02 <earyan02@gmail.com>
2023-08-21 12:07:17 -0400

    fixed js

--- script.js
+++ script.js
@@ -5,15 +5,35 @@ function myFunction() {
     } else {
       infoDiv.style.display = "none";
     }
-}
 
-    function moreLess() {
-
-    let btnText = document.querySelectorAll('#btn').innerText
-
-    if (btnText == 'click for more') {
-        btnText == 'click for less';
+    let button = document.querySelector("#btn");
+  
+    if (button.innerText === "Click for more") {
+      button.innerText = "Click for less";
     } else {
-        btnText = 'click for more';
+      button.innerText = "Click for more";
     }
 }
+
+// function moreLess() {
+//     let button = document.querySelector("#btn");
+  
+//     if (button.innerText === "click for more") {
+//       button.innerText = "click for less";
+//     } else {
+//       button.innerText = "click for more";
+//     }
+//   }
+
+// My original code:
+
+//     function moreLess() {
+
+//     let btnText = document.querySelectorAll('#btn').innerText ((this is a copy of the attribute, this is not selecting the attribute to change))
+
+//     if (btnText == 'click for more') {   << on this line, the "click for more" is not capitalized and so it doesnt match the html, so even in this code btnText does not match
+//         btnText == 'click for less';
+//     } else {
+//         btnText = 'click for more';
+//     }
+// }
